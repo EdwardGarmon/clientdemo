b8b898fc940fb406e23cb2d1cee2e8b8c52eec02
Edward <garmonedward@gmail.com>
2021-05-04 17:12:03 -0500

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Row, Button, Container, Card } from 'react-bootstrap';
+
+
 
 function App() {
+
+
+  const plantNames = [
+    { plant_id: 1, name: 'Luminant Station' },
+    { plant_id: 2, name: 'Leeward Renewable Energy' },
+    { plant_id: 3, name: 'Denison Dam Hydroelectric Power Plant' },
+    { plant_id: 4, name: 'Spencer Generating Station' },
+    { plant_id: 5, name: 'Exelon Power Texas' },
+    { plant_id: 6, name: 'River Bend Station' },
+    { plant_id: 7, name: 'Exelon Power Texas' },
+    { plant_id: 8, name: 'AZZ Wind' },
+    { plant_id: 9, name: 'Panda Power Plant' }
+  ]
+
+  function renderPlantNames() {
+
+    return plantNames.map((plant) => {
+
+      return (
+        <Card>
+          <Card.Body>
+            <Button>
+              {plant.name}
+            </Button>
+          </Card.Body>
+        </Card>
+      )
+
+    })
+
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid >
+        {renderPlantNames()}
+      </Container>
+
     </div>
   );
 }
